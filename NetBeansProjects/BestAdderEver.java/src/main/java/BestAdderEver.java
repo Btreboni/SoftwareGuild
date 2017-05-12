4b269cdfad5b520ad47619457dec1d476c1e7659
Btreboni <btreboni@gmail.com>
2017-05-12 18:44:12 -0400

    BURKETREBONISELFWORK-7: Submitting BestAdderEver.java

---
+++ NetBeansProjects/BestAdderEver.java/src/main/java/BestAdderEver.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author admin
+ */
+
+    // 1. Write a program that stores 3 different numbers in different variables, 
+    //    and displays their values on the screen.
+    // 2. Next add those variables together and store that in a new variable.
+    // 3. Display it. Twice!
+
+
+public class BestAdderEver {
+    public static void main(String[] args) {
+        int apples = 3;
+        int oranges = 2;
+        int mangoes = 6;
+        
+        int fruit = apples + oranges + mangoes; 
+        
+        System.out.println("Fruit equals: " + fruit);
+        System.out.println("Fruit equals: " + fruit);
+        System.out.println("That's two times ");
+    
+    
+    
+    }
+}
